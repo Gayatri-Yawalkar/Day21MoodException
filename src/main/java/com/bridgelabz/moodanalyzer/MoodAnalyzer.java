e288eb94f263b2b99919ddd6cc89a12f52d296f2
unknown <gayoyawalkar@gmail.com>
2021-10-25 06:40:24 +0530

    Added:MoodAnalyzerException Uc3

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,19 +1,22 @@
 package com.bridgelabz.moodanalyzer;
-//Uc2
+//Uc3
 public class MoodAnalyzer {
 	String message;
 	public MoodAnalyzer(String message) {
 		this.message=message;
 	}
-	public String analyzeMood() {
+	public String analyzeMood() throws MoodAnalysisException {
 		try {
+			if(message.length()==0) {
+				throw new MoodAnalysisException("Please Enter Proper Message.Do not enter empty Message",MoodAnalysisException.ExceptionType.ENTERED_EMPTY);
+			}
 			if(message.contains("sad")){
 				return "SAD";
 			} else {
 				return "HAPPY";
 			}
 		} catch(NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalysisException("Please Enter Proper Message",MoodAnalysisException.ExceptionType.ENTERED_NULL);
 		}
 	}
 }
--- src/test/java/com/bridgelabz/junit/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/junit/MoodAnalyzerTest.java
@@ -1,24 +1,72 @@
 package com.bridgelabz.junit;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import com.bridgelabz.moodanalyzer.MoodAnalysisException;
 import com.bridgelabz.moodanalyzer.MoodAnalyzer;
 public class MoodAnalyzerTest {
 	@Test
 	public void givenMessage_whenMoodIsHappy_shouldReturnHappy() {
 		MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am Happy");
+		try {
 		String mood=moodAnalyzer.analyzeMood();
 		assertEquals("HAPPY",mood);
+		} catch(MoodAnalysisException e) {
+			assertEquals("Please Enter Proper Message",e.getMessage());
+			
+		}
+		
 	}
 	@Test
 	public void givenMessage_whenMoodIsSad_shouldReturnSad() {
 		MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am sad");
+		try {
 		String mood=moodAnalyzer.analyzeMood();
 		assertEquals("SAD",mood);
+		} catch(MoodAnalysisException e) {
+			assertEquals("Please Enter Proper Message",e.getMessage());
+			
+		}
+	}
+//null Message
+	@Test
+	public void givenMessage_whenMoodIsInvalid_shouldThrowException() {
+		MoodAnalyzer moodAnalyzer=new MoodAnalyzer(null);
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch(MoodAnalysisException e) {
+			assertEquals("Please Enter Proper Message",e.getMessage());
+			
+		}
 	}
 	@Test
-	public void givenMessage_whenMessageIsNull_shouldReturnHappy() {
+	public void givenMessage_whenMoodIsNull_shouldThrowException() {
 		MoodAnalyzer moodAnalyzer=new MoodAnalyzer(null);
-		String mood=moodAnalyzer.analyzeMood();
-		assertEquals("HAPPY",mood);
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch(MoodAnalysisException e) {
+			assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL,e.type);
+			
+		}
+	}
+//Empty Message
+	@Test
+	public void givenMessage_whenMessageIsEmpty_shouldThrowException() {
+		MoodAnalyzer moodAnalyzer=new MoodAnalyzer("");
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch(MoodAnalysisException e) {
+			assertEquals("Please Enter Proper Message.Do not enter empty Message",e.getMessage());
+			
+		}
+	}
+	@Test
+	public void givenMessage_whenMessageIsNull_shouldThrowException() {
+		MoodAnalyzer moodAnalyzer=new MoodAnalyzer("     ");
+		try {
+			moodAnalyzer.analyzeMood();
+		} catch(MoodAnalysisException e) {
+			assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,e.type);
+			
+		}
 	}
 }
