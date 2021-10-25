d77b9e0f37590327af55484e6a9f0ca09325038c
unknown <gayoyawalkar@gmail.com>
2021-10-25 06:30:05 +0530

    Welcome to Mood Analyzer with Exception Program

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.moodanalyzer;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyzer Program with Custom Exception");
+	}
+}
